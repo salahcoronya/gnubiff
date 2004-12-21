2f6ab99d2b8b5c1d854ac75ad27e5a4d9103e264
Robert Sowada <sowadart@gmx.de>
2004-12-21 16:09:41 +0000

    Ported substituting of '%d' from 2.0.2 to 2.1.0

--- src/ui-applet.cc
+++ src/ui-applet.cc
@@ -19,8 +19,8 @@
 // ========================================================================
 //
 // File          : $RCSfile: ui-applet.cc,v $
-// Revision      : $Revision: 1.4 $
-// Revision date : $Date: 2004/12/03 17:13:39 $
+// Revision      : $Revision: 1.5 $
+// Revision date : $Date: 2004/12/04 20:38:22 $
 // Author(s)     : Nicolas Rougier
 // Short         : 
 //
@@ -99,32 +99,19 @@ Applet::unread_markup (std::string &text)
 	text += biff_->applet_font_;
 	text += "\">";
 
-	std::string ctext;
+	std::vector<std::string> vec(1);
 	if (unread == 0) {
-		ctext = biff_->nomail_text_;
-		guint i;
-		while ((i = ctext.find ("%d")) != std::string::npos) {
-			ctext.erase (i, 2);
-			ctext.insert(i, unreads.str());
-		}
+		vec[0]=std::string(unreads.str());
+		text+=gb_substitute(biff_->nomail_text_,"d",vec);
 	}
 	else if (unread < biff_->max_mail_) {
-		ctext = biff_->newmail_text_;
-		guint i;
-		while ((i = ctext.find ("%d")) != std::string::npos) {
-			ctext.erase (i, 2);
-			ctext.insert(i, unreads.str());
-		}
+		vec[0]=std::string(unreads.str());
+		text+=gb_substitute(biff_->newmail_text_,"d",vec);
 	}
 	else {
-		ctext = biff_->newmail_text_;
-		guint i;
-		while ((i = ctext.find ("%d")) != std::string::npos) {
-			ctext.erase (i, 2);
-			ctext.insert(i, std::string(smax.str().size(), '+'));
-		}
+		vec[0]=std::string(std::string(smax.str().size(), '+'));
+		text+=gb_substitute(biff_->newmail_text_,"d",vec);
 	}
-	text += ctext;
 	text += "</span>";
 	
 	return unread;
