c9872548ea4a692c3a980f9179e6b5c009c01202
Robert Sowada <sowadart@gmx.de>
2005-03-20 21:07:46 +0000

    Bugfix:  * Only update popup when mail is read if it is displayed. Don't show popup    when messages are read but there are no new ones

--- src/ui-applet.cc
+++ src/ui-applet.cc
@@ -19,8 +19,8 @@
 // ========================================================================
 //
 // File          : $RCSfile: ui-applet.cc,v $
-// Revision      : $Revision: 1.13 $
-// Revision date : $Date: 2005/03/10 20:10:02 $
+// Revision      : $Revision: 1.14 $
+// Revision date : $Date: 2005/03/20 20:15:03 $
 // Author(s)     : Nicolas Rougier
 // Short         : 
 //
@@ -175,21 +175,26 @@ Applet::update (gboolean no_popup)
 		unread += biff_->mailbox(i)->unreads();
 	}
 
+	// New mail command
 	if ((newmail == true) && (unread > 0) && (force_popup_ == false)
 		&& (biff_->value_bool ("use_newmail_command"))) {
 		std::string command = biff_->value_string ("newmail_command") + " &";
 		system (command.c_str());
 	}
 
+	// Update popup
 	if (!no_popup && (biff_->popup())) {
 		// If there are no mails to display then hide popup
 		if (!unread && (biff_->value_bool ("use_popup") || force_popup_))
 			biff_->popup()->hide();
 
+		gboolean vis = GTK_WIDGET_VISIBLE (biff_->popup()->get ("dialog"));
+
 		// Otherwise update and display the popup
-		// Note: Must not test for new mail because popup also needs updating
-		// when some messages were read
-		if (unread && ((biff_->value_bool ("use_popup")) || (force_popup_))) {
+		// Note: Must not test for new mail when popup is shown
+		// because it also needs updating when some messages were read
+		if (unread && ((biff_->value_bool ("use_popup")) || (force_popup_))
+			&& (newmail || vis)) {
 			biff_->popup()->update();
 			biff_->popup()->show();
 		}
