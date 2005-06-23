62c4bfddbb60def9a42f4d852a99f4b5a2ee0269
Robert Sowada <sowadart@gmx.de>
2005-06-23 17:31:43 +0000

    Change:  * Default value of option "mailbox/other_port" is now 0 (see discussion on    the gnubiff devel list for more details)

--- src/gnubiff_options.cc
+++ src/gnubiff_options.cc
@@ -19,8 +19,8 @@
 // ========================================================================
 //
 // File          : $RCSfile: gnubiff_options.cc,v $
-// Revision      : $Revision: 1.29 $
-// Revision date : $Date: 2005/04/06 21:38:15 $
+// Revision      : $Revision: 1.30 $
+// Revision date : $Date: 2005/04/07 14:46:53 $
 // Author(s)     : Robert Sowada, Nicolas Rougier
 // Short         : Options for gnubiff
 //
@@ -410,14 +410,14 @@ Gnubiff_Options::add_options_mailbox (void)
 	add_option (new Option_UInt ("other_port", OPTGRP_MAILBOX,
 		"Port to be used when connecting to the server via the internet on "
 		"a non standard port.",
-								   995, OPTFLG_CHANGE, OPTGUI_SPIN,
+								   0, OPTFLG_CHANGE, OPTGUI_SPIN,
 								   "port_spin"));
 	// PORT
 	add_option (new Option_UInt ("port", OPTGRP_MAILBOX,
 		"Port to be used when connecting to the server via the internet. "
 		"If authentication is set to autodetect this option will be set "
 		"when the mailbox is connected for the first time.",
-								 995, OPTFLG_AUTO | OPTFLG_NOSAVE));
+								 0, OPTFLG_AUTO | OPTFLG_NOSAVE));
 	// PROTOCOL
 	add_option (new Option_UInt ("protocol", OPTGRP_MAILBOX,
 		"Protocol to be used by the mailbox.",
