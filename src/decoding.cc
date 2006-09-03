fa9936725c73e102a5e609326fe822052acd934c
Robert Sowada <sowadart@gmx.de>
2006-09-03 14:33:02 +0000

    Bugfix:  * Compatibility with passwords shorter than 16 characters was broken by the    last fixes

--- src/decoding.cc
+++ src/decoding.cc
@@ -19,8 +19,8 @@
 // ========================================================================
 //
 // File          : $RCSfile: decoding.cc,v $
-// Revision      : $Revision: 1.26 $
-// Revision date : $Date: 2006/08/13 22:31:27 $
+// Revision      : $Revision: 1.27 $
+// Revision date : $Date: 2006/08/20 19:01:47 $
 // Author(s)     : Nicolas Rougier, Robert Sowada
 // Short         : Various functions for decoding, converting ...
 //
@@ -812,10 +812,10 @@ Decoding::decrypt_aes (const std::string &passphrase, const std::string &data)
 	// Decrypt via AES
 	AES_KEY aes_key;
 	for (guint i = 0, j = 0; i < size; i+=16) {
-		if (++j > phraselen - 16)
-			j = 0;
 		AES_set_decrypt_key (phraseptr + j, 128, &aes_key);
 		AES_decrypt (bin + i, result + i, &aes_key);
+		if (++j > phraselen - 16)
+			j = 0;
 	}
 
 	// Free memory
@@ -865,10 +865,10 @@ Decoding::encrypt_aes (const std::string &passphrase, const std::string &data)
 	// Encrypt via AES
 	AES_KEY aes_key;
 	for (guint i = 0, j = 0; i < size; i += 16) {
-		if (++j > phraselen - 16)
-			j = 0;
 		AES_set_encrypt_key (phraseptr + j, 128, &aes_key);
 		AES_encrypt (dataptr + i, result + i, &aes_key);
+		if (++j > phraselen - 16)
+			j = 0;
 	}
 
 	// Binary to ASCII
