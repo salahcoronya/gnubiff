62815d65953cac8c761b0309fba7f0987e2f9367
Robert Sowada <sowadart@gmx.de>
2007-04-20 18:21:56 +0000

    Feature:  * Showing/Hiding the popup via SIGUSR signals "popup_show", "popup_hide",    "popup_togglevisible"  * Thanks to Jonathan Hitchcock for providing a patch  * Modifications to the original patch: Using the appletgui cleanup committed       before; don't breaking the system-tray frontend or any other GUI frontend       that may be implemented in the future; don't display empty popups

--- src/ui-applet-gui.cc
+++ src/ui-applet-gui.cc
@@ -19,8 +19,8 @@
 // ========================================================================
 //
 // File          : $RCSfile: ui-applet-gui.cc,v $
-// Revision      : $Revision: 1.15 $
-// Revision date : $Date: 2007/02/04 19:02:44 $
+// Revision      : $Revision: 1.16 $
+// Revision date : $Date: 2007/04/20 17:03:55 $
 // Author(s)     : Nicolas Rougier, Robert Sowada
 // Short         : 
 //
@@ -448,14 +448,12 @@ AppletGUI::update (gboolean init, std::string widget_image,
 	if (!init && (popup_)) {
 		// If there are no mails to display then hide popup
 		if (!unread && (biff_->value_bool ("use_popup") || force_popup_))
-			popup_->hide();
+			hide_dialog_popup ();
 
 		// Update and display the popup
 		if (unread && ((biff_->value_bool ("use_popup")) || force_popup_)
-			&& (newmail || visible_dialog_popup () || force_popup_)) {
-			popup_->update();
-			popup_->show();
-		}
+			&& (newmail || visible_dialog_popup () || force_popup_))
+			show_dialog_popup ();
 	}
 
 	// Update applet's image
@@ -621,8 +619,15 @@ AppletGUI::hide_dialog_about (void)
 void 
 AppletGUI::show_dialog_popup (void)
 {
-	if (popup_)
-		popup_->show ();
+	if (popup_) {
+		// First hide and update the popup
+		hide_dialog_popup ();
+		guint num =	popup_->update ();
+
+		// Show it only if there is at least one header
+		if (num)
+			popup_->show();
+	}
 }
 
 /**
