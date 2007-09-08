c5d7d88ca90fac860f7abcb5bb6e60fe63c2f87a
Robert Sowada <sowadart@gmx.de>
2007-09-08 14:57:39 +0000

    Upgrading license to GPLv3 -- source files

--- src/file.cc
+++ src/file.cc
@@ -2,9 +2,9 @@
 // gnubiff -- a mail notification program
 // Copyright (c) 2000-2007 Nicolas Rougier, 2004-2007 Robert Sowada
 //
-// This program is free software; you can redistribute it and/or
+// This program is free software: you can redistribute it and/or
 // modify it under the terms of the GNU General Public License as
-// published by the Free Software Foundation; either version 2 of the
+// published by the Free Software Foundation, either version 3 of the
 // License, or (at your option) any later version.
 //
 // This program is distributed in the hope that it will be useful, but
@@ -13,14 +13,12 @@
 // General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
-// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
-// 02111-1307, USA.
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // ========================================================================
 //
 // File          : $RCSfile: file.cc,v $
-// Revision      : $Revision: 1.19 $
-// Revision date : $Date: 2006/01/01 16:44:52 $
+// Revision      : $Revision: 1.20 $
+// Revision date : $Date: 2007/07/15 21:00:17 $
 // Author(s)     : Nicolas Rougier, Robert Sowada
 // Short         : 
 //
