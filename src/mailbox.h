bd07adaf01e03cf8128483adc0b3fdb5f7987c33
Robert Sowada <sowadart@gmx.de>
2007-09-08 18:06:31 +0000

    Upgrading license to GPLv3 -- header files

--- src/mailbox.h
+++ src/mailbox.h
@@ -1,10 +1,10 @@
 // ========================================================================
 // gnubiff -- a mail notification program
-// Copyright (c) 2000-2006 Nicolas Rougier, 2004-2006 Robert Sowada
+// Copyright (c) 2000-2007 Nicolas Rougier, 2004-2007 Robert Sowada
 //
-// This program is free software; you can redistribute it and/or
+// This program is free software: you can redistribute it and/or
 // modify it under the terms of the GNU General Public License as
-// published by the Free Software Foundation; either version 2 of the
+// published by the Free Software Foundation, either version 3 of the
 // License, or (at your option) any later version.
 //
 // This program is distributed in the hope that it will be useful, but
@@ -13,14 +13,12 @@
 // General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
-// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
-// 02111-1307, USA.
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // ========================================================================
 //
 // File          : $RCSfile: mailbox.h,v $
-// Revision      : $Revision: 1.59 $
-// Revision date : $Date: 2006/08/05 09:13:10 $
+// Revision      : $Revision: 1.60 $
+// Revision date : $Date: 2006/09/03 18:44:30 $
 // Author(s)     : Nicolas Rougier, Robert Sowada
 // Short         : 
 //
